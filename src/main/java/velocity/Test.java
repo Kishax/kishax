5b9de7c2c332f8ae8813114e55c532ff7cff99fa
bella <maekawatakaya10.8@gmail.com>
2024-10-28 06:03:55 +0900

    DoServerOnline class is done, next is debug, so after this commit, not needed code will be removed

---
+++ src/main/java/velocity/Test.java
@@ -0,0 +1,8 @@
+package velocity;
+
+public class Test {
+    public static void main(String[] args) {
+        Object obj = "aaa";
+        System.out.println(obj.getClass().getSimpleName());
+    }
+}
