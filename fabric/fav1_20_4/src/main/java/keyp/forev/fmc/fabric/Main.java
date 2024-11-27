277d76049c39eb8e1f5a9d8d8762f0f4e433fd51
bella <maekawatakaya10.8@gmail.com>
2024-11-27 16:23:41 +0900

    Removed extra class: Main.java, Main2.java

--- fabric/fav1_20_4/src/main/java/keyp/forev/fmc/fabric/Main.java
+++
@@ -1,8 +0,0 @@
-package keyp.forev.fmc.fabric;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!Main");
-        Main2.main(args);
-    }
-}
