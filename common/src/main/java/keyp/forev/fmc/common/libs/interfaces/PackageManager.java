f8026f0ca8d01731ba524f84d854316712ba622b
bella <maekawatakaya10.8@gmail.com>
2024-11-30 13:29:01 +0900

    ClassManagerのコンストラクタを追加し、VClassManagerを拡張。JDA列挙型を更新し、クラス名とパラメータ型を管理するように変更。PackageManagerインターフェースからgetAllメソッドを削除。

--- common/src/main/java/keyp/forev/fmc/common/libs/interfaces/PackageManager.java
+++ common/src/main/java/keyp/forev/fmc/common/libs/interfaces/PackageManager.java
@@ -1,12 +1,10 @@
 package keyp.forev.fmc.common.libs.interfaces;
 
 import java.net.URL;
-import java.util.EnumSet;
 
 import keyp.forev.fmc.common.libs.ClassManager;
 
 public interface PackageManager {
-    EnumSet<ClassManager> getAll();
     ClassManager getClassManager(ClassManager classManager);
     URL getUrl();
     String getClassName();
