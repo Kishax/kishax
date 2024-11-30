6671c930f3ec9fa24a60c38bc46a5be7977ebbbb
bella <maekawatakaya10.8@gmail.com>
2024-11-30 18:51:08 +0900

    ClassHandlerインターフェースを削除し、ClassManagerおよびVClassManagerの構造を改善。URLClassLoaderの管理をVClassManagerに移行し、パッケージ管理の効率を向上させるための変更を実施。

--- common/src/main/java/keyp/forev/fmc/common/libs/interfaces/PackageManager.java
+++ common/src/main/java/keyp/forev/fmc/common/libs/interfaces/PackageManager.java
@@ -2,10 +2,7 @@ package keyp.forev.fmc.common.libs.interfaces;
 
 import java.net.URL;
 
-import keyp.forev.fmc.common.libs.ClassManager;
-
 public interface PackageManager {
-    ClassManager getClassManager(ClassManager classManager);
     URL getUrl();
     String getClassName();
     String getVersion();
--- velocity/src/main/java/keyp/forev/fmc/velocity/libs/VPackageManager.java
+++ velocity/src/main/java/keyp/forev/fmc/velocity/libs/VPackageManager.java
@@ -1,48 +1,24 @@
 package keyp.forev.fmc.velocity.libs;
 
-import keyp.forev.fmc.common.libs.ClassManager;
 import keyp.forev.fmc.common.libs.interfaces.PackageManager;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.EnumSet;
 
 public enum VPackageManager implements PackageManager {
-    // パッケージから、使うimportリストへの1対1のマップが作れる
-    // Map<PackageList, List<ClassLoader>>?
-    // このマップを作ったときのメリット
-    // 1. パッケージリストから、使うクラスローダーを取得できる
-    JDA("maven", "net.dv8tion:JDA:5.2.0", "net.dv8tion.jda.api.JDA", EnumSet.of(
-        ClassManager.SUB_COMMAND,
-        ClassManager.TEXT_CHANNEL
-    ))
+    JDA("maven", "net.dv8tion:JDA:5.2.0", "net.dv8tion.jda.api.JDA")
     ,;
     private final String repositoryType;
     private final String groupId;
     private final String artifactId;
     private final String version;
     private final String className;
-    private final EnumSet<ClassManager> classManagers;
-    VPackageManager(String repositryType, String coordinates, String className, EnumSet<ClassManager> classManagers) {
+    VPackageManager(String repositryType, String coordinates, String className) {
         this.repositoryType = repositryType;
         String[] parts = coordinates.split(":");
         this.groupId = parts[0];
         this.artifactId = parts[1];
         this.version = parts[2];
         this.className = className;
-        this.classManagers = classManagers;
-    }
-
-    @Override
-    public EnumSet<ClassManager> getAll() {
-        return classManagers;
-    }
-
-    @Override
-    public ClassManager getClassManager(ClassManager classManager) {
-        return classManagers.stream()
-            .filter(cm -> cm.equals(classManager))
-            .findFirst()
-            .orElse(null);
     }
 
     @Override
