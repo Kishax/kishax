71c93f507ad9fe9cfa64e42fff2b1929b5ba9ba2
bella <maekawatakaya10.8@gmail.com>
2024-12-01 14:09:53 +0900

    ClassManagerおよびVClassManagerの構造を改善し、URLClassLoaderの管理を統合。不要なコメントを削除し、コンストラクタの引数を整理。

--- common/src/main/java/keyp/forev/fmc/common/libs/ClassManager.java
+++ common/src/main/java/keyp/forev/fmc/common/libs/ClassManager.java
@@ -4,46 +4,29 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URLClassLoader;
 
-// わからないことが一点あって、それは、
-// この場合、ClassManagerクラスから、
-// (interface) >> VClassManagerクラス >> (enum JDA) >> (JDAの)ClassManagerクラス
-// が得られるが、これは双方向からもいけるのかどうか？
-// それによって、毎回、どちらかのクラスから、クラスをロードするかを決める必要があり、
-// 現在、ClassManagerクラスとVClassManagerクラスでは、複数コンストラクタを使っているが
-// それの結果によって、どちらかを削除する必要がある
-// 予想は、VClassManagerクラスから、取得したほうが良いのかもしれない。
-// なぜなら、VClassManagerクラスは、ClassManagerクラスを継承しているため。
-// 一度、毎回、VClassManagerクラスから取得することを試してみるため、
-// したのこのクラスでのurlClassLoaderはコメントアウトし、VClassManagerクラスで管理するものとする
-public class ClassManager<T> {
-    protected Class<T> clazz;
-    protected Class<?>[] parameterTypes;
-    //protected URLClassLoader urlClassLoader;
-
-    // 普通にこのクラスにアクセスするときは、
-    // VClassManager<JDA> manager = new VClassManager<>(JDA.class, new Class<?>[]{}, urlClassLoader);
-    // このようにして、取得が可能になる
-    // しかし、そんなことをしなくても、
-    // このクラスを継承しているVClassManagerを媒介にするか、
-    // このクラスを継承しているEnumを媒介にすることで、
+public class ClassManager {
+    private Class<?> clazz;
+    private Class<?>[] parameterTypes;
+    // private URLClassLoader urlClassLoader;
+    // このクラスにアクセスするときは、
+    // このクラスを継承しているEnumクラス郡を媒介にすることで、
     // このクラスを取得することができる
     // 例として、velocity/Mainクラスに記載
-    public ClassManager(Class<T> clazz, Class<?>[] parameterTypes/*, URLClassLoader urlClassLoader*/) {
+    public ClassManager(Class<?> clazz, Class<?>[] parameterTypes) {
         this.clazz = clazz;
         this.parameterTypes = parameterTypes;
-        /*this.urlClassLoader = urlClassLoader;*/
     }
 
     // 複数コンストラクタで、URLClassLoaderを使う場合
     public ClassManager(URLClassLoader urlClassLoaderBase) {
-        this(null, null/*, null*/);
+        this(null, null);
     }
 
-    public Class<T> getClazz() {
+    public Class<?> getClazz() {
         return clazz;
     }
 
-    public Constructor<T> getConstructor() {
+    public Constructor<?> getConstructor() {
         try {
             return clazz.getConstructor(parameterTypes);
         } catch (NoSuchMethodException e) {
@@ -52,9 +35,9 @@ public class ClassManager<T> {
         }
     }
 
-    public T createInstance(Object... initargs) {
+    public Object createInstance(Object... initargs) {
         try {
-            Constructor<T> constructor = getConstructor();
+            Constructor<?> constructor = getConstructor();
             if (constructor != null) {
                 return constructor.newInstance(initargs);
             }
@@ -63,21 +46,4 @@ public class ClassManager<T> {
         }
         return null;
     }
-
-    public interface JDA {
-        // VClassManagerからClassManagerのインスタンスを取得するならば必要ない
-        ClassManager<SubCommand> SUB_COMMAND = new ClassManager<>(SubCommand.class, new Class<?>[]{String.class, String.class});
-        //ClassManager<TextChannel> TEXT_CHANNEL = new ClassManager<>(TextChannel.class, new Class<?>[]{String.class}, null);
-        // 継承先のクラスで定義されたパッケージ情報の変数を使い、ClassManagerクラスを生成
-        ClassManager<?> get() throws ClassNotFoundException;
-    }
-
-    // VClassManagerからClassManagerのインスタンスを取得するならば必要ない
-    public enum SubCommand {
-        COMMAND1, COMMAND2;
-    }
-
-    //public enum TextChannel {
-    //    CHANNEL1, CHANNEL2;
-    //}
 }
