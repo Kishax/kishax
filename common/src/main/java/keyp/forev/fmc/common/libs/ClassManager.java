4417d6872629e5b86b932138ef824d9bb520f680
bella <maekawatakaya10.8@gmail.com>
2024-12-03 00:23:16 +0900

    ClassManagerとVClassManagerのコンストラクタからparameterTypesを削除し、シンプルな引数に変更。JDAおよびCLUB_MINNCED列挙型の初期化を簡素化。

--- common/src/main/java/keyp/forev/fmc/common/libs/ClassManager.java
+++ common/src/main/java/keyp/forev/fmc/common/libs/ClassManager.java
@@ -1,8 +1,6 @@
 package keyp.forev.fmc.common.libs;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URLClassLoader;
 import java.util.Map;
 
@@ -11,20 +9,18 @@ import keyp.forev.fmc.common.libs.interfaces.PackageManager;
 public class ClassManager {
     public static Map<PackageManager, URLClassLoader> urlClassLoaderMap;
     private Class<?> clazz;
-    private Class<?>[] parameterTypes;
     // private URLClassLoader urlClassLoader;
     // このクラスにアクセスするときは、
     // このクラスを継承しているEnumクラス郡を媒介にすることで、
     // このクラスを取得することができる
     // 例として、velocity/Mainクラスに記載
-    public ClassManager(Class<?> clazz, Class<?>[] parameterTypes) {
+    public ClassManager(Class<?> clazz) {
         this.clazz = clazz;
-        this.parameterTypes = parameterTypes;
     }
 
     // 複数コンストラクタで、URLClassLoaderを使う場合
     public ClassManager(URLClassLoader urlClassLoaderBase) {
-        this(null, null);
+        this((Class<?>) null);
     }
 
     public Class<?> getClazz() {
@@ -38,26 +34,5 @@ public class ClassManager {
             e.printStackTrace();
             return null;
         }
-    }
-    
-    public Constructor<?> getConstructor() {
-        try {
-            return clazz.getConstructor(parameterTypes);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public Object getInstance(Object... initargs) {
-        try {
-            Constructor<?> constructor = getConstructor();
-            if (constructor != null) {
-                return constructor.newInstance(initargs);
-            }
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
--- velocity/src/main/java/keyp/forev/fmc/velocity/libs/VClassManager.java
+++ velocity/src/main/java/keyp/forev/fmc/velocity/libs/VClassManager.java
@@ -6,50 +6,49 @@ import keyp.forev.fmc.common.libs.ClassManager;
 
 public class VClassManager {
     public enum JDA {
-        JDA_BUILDER("net.dv8tion.jda.api.JDABuilder", new Class<?>[]{}),
-        GATEWAY_INTENTS("net.dv8tion.jda.api.requests.GatewayIntent", new Class<?>[]{int.class}),
-        SUB_COMMAND("net.dv8tion.jda.api.interactions.commands.build.SubcommandData", new Class<?>[]{String.class, String.class}),
-        TEXT_CHANNEL("net.dv8tion.jda.api.entities.channel.concrete.TextChannel", new Class<?>[]{String.class}),
-        OPTION_DATA("net.dv8tion.jda.api.interactions.commands.build.OptionData", new Class<?>[]{String.class, String.class}),
-        OPTION_TYPE("net.dv8tion.jda.api.interactions.commands.OptionType", new Class<?>[]{int.class}),
-        RESTACTION_MESSAGE("net.dv8tion.jda.api.entities.channel.middleman.MessageChannel", new Class<?>[]{String.class}),
-        ENTITYS_MESSAGE("net.dv8tion.jda.api.entities.Message", new Class<?>[]{String.class}),
-        ENTITYS_ACTIVITY("net.dv8tion.jda.api.entities.Activity", new Class<?>[]{String.class}),
-        BUTTON("net.dv8tion.jda.api.interactions.components.buttons.Button", new Class<?>[]{String.class, String.class}),
-        PRESENCE("net.dv8tion.jda.api.managers.Presence", new Class<?>[]{String.class}),
+        JDA_BUILDER("net.dv8tion.jda.api.JDABuilder"),
+        EMBED_BUILDER("net.dv8tion.jda.api.EmbedBuilder"),
+        GATEWAY_INTENTS("net.dv8tion.jda.api.requests.GatewayIntent"),
+        SUB_COMMAND("net.dv8tion.jda.api.interactions.commands.build.SubcommandData"),
+        TEXT_CHANNEL("net.dv8tion.jda.api.entities.channel.concrete.TextChannel"),
+        OPTION_DATA("net.dv8tion.jda.api.interactions.commands.build.OptionData"),
+        OPTION_TYPE("net.dv8tion.jda.api.interactions.commands.OptionType"),
+        RESTACTION_MESSAGE("net.dv8tion.jda.api.entities.channel.middleman.MessageChannel"),
+        ENTITYS_MESSAGE("net.dv8tion.jda.api.entities.Message"),
+        ENTITYES_MESSAGE_EMBED("net.dv8tion.jda.api.entities.MessageEmbed"),
+        ENTITYS_ACTIVITY("net.dv8tion.jda.api.entities.Activity"),
+        BUTTON("net.dv8tion.jda.api.interactions.components.buttons.Button"),
+        PRESENCE("net.dv8tion.jda.api.managers.Presence"),
+        ERROR_RESPONSE_EXCEPTION("net.dv8tion.jda.api.exceptions.ErrorResponseException"),
         ;
         private String clazzName;
-        private Class<?>[] parameterTypes;
         private URLClassLoader urlClassLoader;
-        JDA(String clazzName, Class<?>[] parameterTypes) {
+        JDA(String clazzName) {
             this.clazzName = clazzName;
-            this.parameterTypes = parameterTypes;
             if (ClassManager.urlClassLoaderMap != null) {
                 this.urlClassLoader = ClassManager.urlClassLoaderMap.get(VPackageManager.JDA);
             }
         }
         public ClassManager get() throws ClassNotFoundException {
             Class<?> clazz = Class.forName(clazzName, true, urlClassLoader);
-            return new ClassManager(clazz, parameterTypes);
+            return new ClassManager(clazz);
         }
     }
     public enum CLUB_MINNCED {
-        WEBHOOK_CLIENT("club.minnced.discord.webhook.WebhookClient", new Class<?>[]{}),
-        WEBHOOK_MESSAGE("club.minnced.discord.webhook.send.WebhookMessage", new Class<?>[]{String.class}),
+        WEBHOOK_CLIENT("club.minnced.discord.webhook.WebhookClient"),
+        WEBHOOK_MESSAGE("club.minnced.discord.webhook.send.WebhookMessage"),
         ;
         private String clazzName;
-        private Class<?>[] parameterTypes;
         private URLClassLoader urlClassLoader;
-        CLUB_MINNCED(String clazzName, Class<?>[] parameterTypes) {
+        CLUB_MINNCED(String clazzName) {
             this.clazzName = clazzName;
-            this.parameterTypes = parameterTypes;
             if (ClassManager.urlClassLoaderMap != null) {
                 this.urlClassLoader = ClassManager.urlClassLoaderMap.get(VPackageManager.CLUB_MINNCED);
             }
         }
         public ClassManager get() throws ClassNotFoundException {
             Class<?> clazz = Class.forName(clazzName, true, urlClassLoader);
-            return new ClassManager(clazz, parameterTypes);
+            return new ClassManager(clazz);
         }
     }
 }
