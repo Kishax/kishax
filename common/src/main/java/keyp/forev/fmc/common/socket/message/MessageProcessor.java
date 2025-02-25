91f6e3322b4e0ae7d57e358972a56190b5620f07
bella <maekawatakaya10.8@gmail.com>
2025-02-25 13:47:39 +0900

    distinguish message case by case

---
+++ common/src/main/java/keyp/forev/fmc/common/socket/message/MessageProcessor.java
@@ -0,0 +1,61 @@
+package keyp.forev.fmc.common.socket.message;
+
+import java.util.Optional;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.ProvisionException;
+
+import keyp.forev.fmc.common.socket.message.handlers.interfaces.discord.*;
+import keyp.forev.fmc.common.socket.message.handlers.interfaces.web.*;
+import keyp.forev.fmc.common.socket.message.handlers.interfaces.minecraft.commands.*;
+import keyp.forev.fmc.common.socket.message.handlers.interfaces.minecraft.*;
+
+public class MessageProcessor {
+    private final Injector injector;
+
+    @Inject
+    public MessageProcessor(Injector injector) {
+        this.injector = injector;
+    }
+
+    public void process(Message msg) throws ProvisionException {
+        Optional.ofNullable(msg.web)
+                .map(web -> web.confirm)
+                .ifPresent(confirm -> injector.getInstance(MinecraftWebConfirmHandler.class).handle(confirm));
+
+        Optional.ofNullable(msg.discord)
+                .map(discord -> discord.rulebook)
+                .ifPresent(rulebook -> injector.getInstance(RuleBookSyncHandler.class).handle(rulebook));
+
+        if (msg.mc != null) {
+            Optional.ofNullable(msg.mc.server)
+                    .ifPresent(server -> {
+                        injector.getInstance(ServerActionHandler.class).handle(server);
+                    });
+
+            Optional.ofNullable(msg.mc.sync)
+                    .ifPresent(sync -> {
+                        injector.getInstance(SyncContentHandler.class).handle(sync);
+                    });
+
+            Optional.ofNullable(msg.mc.cmd)
+                    .ifPresent(cmd -> {
+                        Optional.ofNullable(cmd.teleport)
+                                .ifPresent(teleport -> {
+                                    Optional.ofNullable(teleport.point)
+                                            .ifPresent(point -> injector.getInstance(TeleportPointHandler.class).handle(point));
+
+                                    Optional.ofNullable(teleport.player)
+                                            .ifPresent(player -> injector.getInstance(TeleportPlayerHandler.class).handle(player));
+                                });
+
+                        Optional.ofNullable(cmd.forward)
+                                .ifPresent(forward -> injector.getInstance(ForwardHandler.class).handle(forward));
+
+                        Optional.ofNullable(cmd.input)
+                                .ifPresent(input -> injector.getInstance(InputHandler.class).handle(input));
+                    });
+        }
+    }
+}
