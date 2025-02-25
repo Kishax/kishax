e992429d03e84190404ae5b86bf58f58a78596c4
bella <maekawatakaya10.8@gmail.com>
2025-02-25 15:10:13 +0900

    remove imports, not used

--- velocity/src/main/java/keyp/forev/fmc/velocity/server/events/EventListener.java
+++ velocity/src/main/java/keyp/forev/fmc/velocity/server/events/EventListener.java
@@ -10,7 +10,6 @@ import java.sql.Timestamp;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
